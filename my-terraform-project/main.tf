af57514064dd88cd28e6939c8bc956d040c892ec
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2025-12-24 06:05:35 +0200

    Fix: storage account name to meet Azure requirements

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "rg-terraform-maxim-init"
+  name                     = "tfmaximstorage01"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
